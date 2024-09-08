c5dce64e04331689b149212e606a7aadffe74119
ducmanh1451 <ducmanh1451>
2024-09-08 16:59:44 +0700

    init FE

---
+++ 01_FE/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline text-gray-500">
+    Hello Vue 3 with TailwindCSS!
+  </h1>
+  <RouterView />
+</template>
